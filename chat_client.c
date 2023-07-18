9168b61f9a06def6390cbe20df7514c59e6df19d
Ritesh Kumar Yadav <siltaz@proton.me>
2023-07-18 23:16:54 +0530

    chat server completed

--- chat_client.c
+++ chat_client.c
@@ -3,59 +3,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define PORT "65001"
+#define TRUE 1
+#define FALSE 0
 
-int main() {
+int main(int argc, char *argv[]) {
 
-  struct addrinfo hints, *host;
-  int sockfd, r;
-  char buffer[BUFSIZ], input[BUFSIZ];
+  struct addrinfo hints, *server;
+  int sockfd, r, done = FALSE;
+  char buffer[BUFSIZ], *host, *port;
+  fd_set read_fd;
 
+  if (argc < 2) {
+    fprintf(stderr, "Format: client hostname\n");
+    exit(1);
+  }
+  host = argv[1];
+  port = argv[2];
+
+  printf("Looking for chat server on %s:%s...", host, port);
   memset_s(&hints, sizeof(hints), 0, sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
 
-  if (getaddrinfo(0, PORT, &hints, &host) < 0) {
+  if (getaddrinfo(0, port, &hints, &server) < 0) {
     perror("error");
     exit(1);
   }
+  puts("found");
 
-  sockfd = socket(host->ai_family, host->ai_socktype, host->ai_protocol);
+  sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
   if (sockfd < 0) {
     perror("error");
     exit(1);
   }
 
-  if (connect(sockfd, host->ai_addr, host->ai_addrlen) < 0) {
+  if (connect(sockfd, server->ai_addr, server->ai_addrlen) < 0) {
     perror("error");
     exit(1);
   }
+  freeaddrinfo(server);
 
-  while (1) {
-    r = recv(sockfd, buffer, BUFSIZ, 0);
-    if (r < 1) {
-      puts("Connection closed by peer\n");
-      break;
+  while (!done) {
+    FD_ZERO(&read_fd);
+    FD_SET(sockfd, &read_fd);
+    FD_SET(0, &read_fd);
+
+    if (select(sockfd + 1, &read_fd, NULL, NULL, 0) < 0) {
+      perror("error");
+      exit(1);
     }
 
-    buffer[r] = '\0';
-    printf("%s\n", buffer);
-
-    printf("Command: ");
-    fgets(input, BUFSIZ, stdin);
-    if (strncmp(input, "close", 5) == 0) {
-      puts("Closing connection");
-      break;
+    if (FD_ISSET(sockfd, &read_fd)) {
+      r = recv(sockfd, buffer, BUFSIZ, 0);
+      if (r < 1) {
+        puts("Connection closed by peer");
+        break;
+      }
+      buffer[r] = '\0';
+      printf("%s", buffer);
     }
 
-    send(sockfd, input, strlen(input), 0);
+    if (FD_ISSET(0, &read_fd)) {
+      if (fgets(buffer, BUFSIZ, stdin) == NULL) {
+        putchar('\n');
+      } else if (strcmp(buffer, "close\n") == 0) {
+        done = TRUE;
+      } else {
+        send(sockfd, buffer, strlen(buffer), 0);
+      }
+    }
   }
 
-  freeaddrinfo(host);
+  printf("Disconnected\nBye!\n");
   close(sockfd);
-  puts("Disconnected");
+
   return 0;
 }
--- chat_server.c
+++ chat_server.c
@@ -13,14 +13,13 @@
 #define FALSE 0
 
 int main() {
-  const int backlog = 10, hostname_size = 32;
-  const char *welcome_msg = "Type 'close' to disconnect; 'shutdown' to stop\n";
-  socklen_t client_len = sizeof(struct sockaddr);
+  const int backlog = 10, clientname_size = 32;
+  socklen_t address_len = sizeof(struct sockaddr);
   struct addrinfo hints, *server;
-  struct sockaddr client;
-  int sockfd, clientfd, max_connect, fd, r, done = FALSE;
-  char hostname[hostname_size], connection[backlog][hostname_size];
-  char buffer[BUFSIZ];
+  struct sockaddr address;
+  int serverfd, clientfd, max_connect, fd, r, x, done = FALSE;
+  char clientname[clientname_size], connection[backlog][clientname_size];
+  char buffer[BUFSIZ], send_str[BUFSIZ];
   fd_set main_fd, read_fd;
 
   memset_s(&hints, sizeof(hints), 0, sizeof(hints));
@@ -33,26 +32,27 @@ int main() {
     exit(1);
   }
 
-  sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
-  if (sockfd < 0) {
+  serverfd =
+      socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+  if (serverfd < 0) {
     perror("error");
     exit(1);
   }
 
-  if (bind(sockfd, server->ai_addr, server->ai_addrlen) < 0) {
+  if (bind(serverfd, server->ai_addr, server->ai_addrlen) < 0) {
     perror("error");
     exit(1);
   }
 
   printf("Server started listening on %s...\n", PORT);
-  if (listen(sockfd, backlog) < 0) {
+  if (listen(serverfd, backlog) < 0) {
     perror("error");
     exit(1);
   }
 
   max_connect = backlog;
   FD_ZERO(&main_fd);
-  FD_SET(sockfd, &main_fd);
+  FD_SET(serverfd, &main_fd);
 
   while (!done) {
     // backup
@@ -65,44 +65,79 @@ int main() {
 
     for (fd = 1; fd <= max_connect; fd++) {
       if (FD_ISSET(fd, &read_fd)) {
-        if (fd == sockfd) {
+        if (fd == serverfd) {
           // accept new connection
-          clientfd = accept(fd, &client, &client_len);
+          clientfd = accept(fd, &address, &address_len);
           if (clientfd < 0) {
             perror("error");
             exit(1);
           }
 
-          if (getnameinfo(&client, client_len, hostname, hostname_size, 0, 0,
-                          NI_NUMERICHOST) < 0) {
+          if (getnameinfo(&address, address_len, clientname, clientname_size, 0,
+                          0, NI_NUMERICHOST) < 0) {
             perror("error");
             exit(1);
           }
 
-          strcpy(connection[clientfd], hostname);
+          strcpy(connection[clientfd], clientname);
           printf("New connection from %s\n", connection[clientfd]);
 
           FD_SET(clientfd, &main_fd);
 
           // response to new connection
-          strcpy(buffer, "Hello to ");
-          strcpy(buffer, connection[clientfd]);
-          strcpy(buffer, "!\n");
-          strcpy(buffer, welcome_msg);
+          strcpy(buffer, "SERVER> Welcome ");
+          strcat(buffer, connection[clientfd]);
+          strcat(buffer, "to the chat server\n");
+          strcat(buffer,
+                 "SERVER> Type 'close' to disconnect; 'shutdown' to stop\n");
           send(clientfd, buffer, strlen(buffer), 0);
+
+          strcpy(buffer, "SERVER> ");
+          strcat(buffer, connection[clientfd]);
+          strcat(buffer, " has joined the server\n");
+
+          for (x = serverfd; x <= max_connect; x++) {
+            if (FD_ISSET(x, &main_fd)) {
+              send(x, buffer, strlen(buffer), 0);
+            }
+          }
+
+          printf("%s", buffer);
         } else {
           // handle existing connection
           r = recv(fd, buffer, BUFSIZ, 0);
           if (r < 1) {
             FD_CLR(fd, &main_fd);
             close(fd);
-            printf("%s closed\n", connection[fd]);
+
+            strcpy(buffer, "SERVER> ");
+            strcat(buffer, connection[fd]);
+            strcat(buffer, " disconnected\n");
+
+            for (x = serverfd; x <= max_connect; x++) {
+              if (FD_ISSET(x, &main_fd)) {
+                send(x, buffer, strlen(buffer), 0);
+              }
+            }
+
+            printf("%s", buffer);
           } else {
             buffer[r] = '\0';
+
             if (strcmp(buffer, "shutdown\n") == 0) {
               done = TRUE;
             } else {
-              send(fd, buffer, strlen(buffer), 0);
+              strcpy(send_str, connection[fd]);
+              strcat(send_str, "> ");
+              strcat(send_str, buffer);
+
+              for (x = serverfd; x <= max_connect; x++) {
+                if (FD_ISSET(x, &main_fd)) {
+                  send(x, send_str, strlen(send_str), 0);
+                }
+              }
+
+              printf("%s", send_str);
             }
           }
         }
@@ -112,7 +147,7 @@ int main() {
 
   puts("Chat Server shutting down\n");
   freeaddrinfo(server);
-  close(sockfd);
+  close(serverfd);
 
   return 0;
 }
